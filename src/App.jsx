3542e1cd10d94b704b9c3b448573dc4b73002382
Rokon Khan <rokonjust@gmail.com>
2025-02-20 20:43:32 +0600

    The Task Management App Set-Up Done with Firebase

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Task Management App</h1>
+      <button className="btn btn-primary">Add To DO</button>
+    </>
+  );
+}
+
+export default App;
